92da5bda5c5e6c936b4fcf5eaedcd1ba4e4e145f
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2024-07-10 21:58:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ async function displayAlbums() {
 
 async function main() {
     // Get the list of all the songs
-    await getSongs("songs/ncs")
+    await getSongs("songs")
     playMusic(songs[0], true)
 
     // Display all the albums on the page
